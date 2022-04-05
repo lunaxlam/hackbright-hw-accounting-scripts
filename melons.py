7ac01703e5ab2d405e9b7527ffde40d7157e64ba
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-04 20:28:51 -0700

    Added refactor_melons_tuple() function

--- melons.py
+++ melons.py
@@ -58,12 +58,16 @@ def refactor_melons(melon_data, melon_attribute):
     :return melon-data: an updated melon dictionary 
     """
 
+    # Iterate through each key in melon_attribute
     for key in melon_attribute:
         if key not in melon_data:
+            # Initialize the key as the value within a list
             melon_data[key] = [melon_attribute[key]]
         else:
+            # Combine the current value (which is a list) with a list of the key of the melon_attribute
             melon_data[key] += [melon_attribute[key]]
 
+    # Return melon_data to update the melon_data variable existing in the outer scope
     return melon_data
 
 refactor_melons(all_melon_data, melon_names)
@@ -74,3 +78,35 @@ refactor_melons(all_melon_data, melon_rind_color)
 refactor_melons(all_melon_data, melon_average_weight)
 
 print(all_melon_data)
+
+melon_data_tuples = {}
+
+def refactor_melons_tuple(melon_data, melon_attribute):
+    """
+    Update/reassign key, value data in master melon dictionary based on melon attribute.
+
+    :param melon_data: a dictionary to store melon attribute key, value data
+    :param melon_attribute: a dictionary containing melon attribute key, value data
+    :return melon-data: an updated melon dictionary 
+    """
+
+    # Iterate through each key in melon_attribute
+    for key in melon_attribute:
+        if key not in melon_data:
+            # Initialize the key as the value within a list
+            melon_data[key] = (melon_attribute[key],)
+        else:
+            # Combine the current value (which is a list) with a list of the key of the melon_attribute
+            melon_data[key] += (melon_attribute[key], )
+
+    # Return melon_data to update the melon_data variable existing in the outer scope
+    return melon_data
+
+refactor_melons_tuple(melon_data_tuples, melon_names)
+refactor_melons_tuple(melon_data_tuples, melon_prices)
+refactor_melons_tuple(melon_data_tuples, melon_seedlessness)
+refactor_melons_tuple(melon_data_tuples, melon_flesh_color)
+refactor_melons_tuple(melon_data_tuples, melon_rind_color)
+refactor_melons_tuple(melon_data_tuples, melon_average_weight)
+
+print(melon_data_tuples)
